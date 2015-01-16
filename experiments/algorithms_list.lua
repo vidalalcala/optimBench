e008bdef42c8396fc7410a7dad8d5ceec84b79c6
Jose V. Alcala-Burgos <vidal.alcala@gmail.com>
2015-01-16 09:03:24 -0600

    reduce the number of parameter configurations, use qlua

--- experiments/algorithms_list.lua
+++ experiments/algorithms_list.lua
@@ -3,28 +3,28 @@ require 'torch'
 
 --[[ A table of different configurations for the algorithms' parameters ]]--
 local parameters={
-	learningRate= {1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1},
-	learningRateDecay= {0.01, 0.1, 0.5, 1},
-	momentum= {0.1, 0.5, 0.9, 0.99, 0.999},
-	lambda= {1e-4, 1e-3, 1e-2, 1e-1, 0.5},
-	alpha= {0.5, 0.75, 1.0},
-	decay= {1-1e-4, 1-1e-3, 1-1e-2, 1-1e-1, 1-0.5},
-	maxLearningRate= {10, 100, 1000},
-	sgdLearningRate= torch.linspace(-10, 0, 34):mul(math.log(10)):exp():totable(),
-	epsilon= {1e-6, 1e-3}
+	--learningRate= {1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1},
+	--learningRateDecay= {0.01, 0.1, 0.5, 1},
+	--momentum= {0.1, 0.5, 0.9, 0.99, 0.999},
+	--lambda= {1e-4, 1e-3, 1e-2, 1e-1, 0.5},
+	--alpha= {0.5, 0.75, 1.0},
+	--decay= {1-1e-4, 1-1e-3, 1-1e-2, 1-1e-1, 1-0.5},
+	--maxLearningRate= {10, 100, 1000},
+	sgdLearningRate= torch.linspace(-5, 0, 4):mul(math.log(10)):exp():totable(),
+	--epsilon= {1e-6, 1e-3}
 }
 parameters.stepsize = parameters.learningRate
 
 --[[ A table of all the algorithms to be considered ]]--
 local algorithms={
 	sgd= {fun='sgd', learningRate=parameters.sgdLearningRate},
-	sgd_annealing= {fun='sgd', learningRate=parameters.learningRate, learningRateDecay=parameters.learningRateDecay},
-	sgd_momentum= {fun='sgd', learningRate=parameters.learningRate, momentum=parameters.momentum},
-	sgd_averaging= {fun='asgd', eta0=parameters.learningRate, lambda=parameters.lambda, alpha=parameters.alpha, t0=0},
+	--sgd_annealing= {fun='sgd', learningRate=parameters.learningRate, learningRateDecay=parameters.learningRateDecay},
+	--sgd_momentum= {fun='sgd', learningRate=parameters.learningRate, momentum=parameters.momentum},
+	--sgd_averaging= {fun='asgd', eta0=parameters.learningRate, lambda=parameters.lambda, alpha=parameters.alpha, t0=0},
 	--sgd_nesterov= {fun='nesterov', learningRate=parameters.learningRate, momentum=parameters.momentum},
 	--rmsprop= {fun='rmsprop', learningRate=parameters.learningRate, decay=parameters.decay, maxLearningRate=parameters.maxLearningRate},
-	rprop= {fun='rprop', stepsize=parameters.stepsize},
-	adagrad= {fun='adagrad', learningRate=parameters.learningRate},
+	--rprop= {fun='rprop', stepsize=parameters.stepsize},
+	--adagrad= {fun='adagrad', learningRate=parameters.learningRate},
 	--adadelta= {fun='adadelta', decay=parameters.decay, epsilon=parameters.epsilon},
 	--cg= {fun='cg'},
 	--idbd= {fun='idbd'}
